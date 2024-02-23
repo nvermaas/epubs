9a7d198bc28a70603c2ac6a6a350e8fb57eb9cba
Vermaas <vermaas@astron.nl>
2024-02-23 17:27:34 +0100

    make it OO

--- epubs.py
+++ epubs.py
@@ -2,95 +2,118 @@ import os,sys
 import argparse
 import pypdf
 
-import ebooklib
 from ebooklib import epub
 
+class MyPDF:
+    def __init__(self, args):
+        self.reader = pypdf.PdfReader(args.input)
+
+    def extract_text(self):
+        chapter_text = ""
+
+        count = 0
+        for page in self.reader.pages:
+
+            count += 1
+
+            # extracting text from page
+            page_text = page.extract_text()
+
+            # replace \n with <br>
+            page_text = page_text.replace('\n', '<br>')
+
+            # remove the header of every original pdf page
+            if count > 1:
+                page_text = page_text.split('<br>', 1)[1]
+
+            # remove the footer and paging of the original pdf (everything after the last <br>)
+            page_text = page_text.rsplit('<br>', 1)[0]
+
+            chapter_text = chapter_text + page_text
+
+        print(chapter_text)
+        return chapter_text
+
+
+class MyBook:
+    def __init__(self, args):
+        self.book = epub.EpubBook()
+        self.book.set_title(args.title)
+        self.book.set_language("en")
+        self.book.add_author("NASA Oral Histories")
+        self.book.set_cover(args.cover_image, "NASA Oral Histories")
+
+        # create image from the local image
+        image_content = open(args.cover_image, "rb").read()
+        self.img = epub.EpubImage(
+            uid="image_1",
+            file_name="static/cover.jpg",
+            media_type="image/gif",
+            content=image_content,
+        )
+        self.spine = ['nav']
+        self.toc = []
+
+    def add_chapter(self, title, chapter_text):
+
+        # create chapter
+        count = len(self.spine)
+        filename = f"chapter_{count}.xhtml"
+
+        c = epub.EpubHtml(title=title, file_name=filename, lang="en")
+        c.content = (
+                "<h1>" + title + "</h1>"
+                "<p>" + chapter_text + "</p>"
+        )
+        self.book.add_item(c)
+        self.spine.append(c)
+
+        # keep table of contents
+        toc_object = epub.Link(filename,title,title)
+        self.toc.append(toc_object)
+
+    def save(self, args):
+        # generate Table Of Contents
+        self.book.toc = tuple(self.toc)
+        self.book.add_item(self.img)
+
+        # add default NCX and Nav file
+        self.book.add_item(epub.EpubNcx())
+        self.book.add_item(epub.EpubNav())
+
+        # define CSS style
+        style = "BODY {color: white;}"
+        nav_css = epub.EpubItem(
+            uid="style_nav",
+            file_name="style/nav.css",
+            media_type="text/css",
+            content=style,
+        )
+
+        # add CSS file
+        self.book.add_item(nav_css)
+
+        # basic spine
+        self.book.spine = self.spine
+
+        # write to the file
+        epub.write_epub(args.output, self.book, {})
+
+#========================================================================
 def pdf_to_epub(args):
 
-    # creating a pdf reader object
-    reader  = pypdf.PdfReader(args.input)
+    # open the pdf and extract the text
+    myPDF = MyPDF(args)
+    chapter_text = myPDF.extract_text()
 
-    chapter_text = ""
+    # create the epub
+    myBook = MyBook(args)
 
-    count = 0
-    for page in reader.pages:
+    # add the extracted text from the pdf as a chapter
+    myBook.add_chapter(args.title, chapter_text)
 
-        count+=1
-
-        # extracting text from page
-        page_text = page.extract_text()
-
-        # replace \n with <br>
-        page_text = page_text.replace('\n','<br>')
-
-        # remove the header of every original pdf page
-        if count>1:
-            page_text = page_text.split('<br>',1)[1]
-
-        # remove the footer and paging of the original pdf (everything after the last <br>)
-        page_text = page_text.rsplit('<br>', 1)[0]
-
-        chapter_text = chapter_text + page_text
-        print(page_text)
-        print('--------------------------------------')
-
-    # create the epub file
-    book = epub.EpubBook()
-    book.set_title(args.title)
-    book.set_language("en")
-    book.add_author("NASA Oral Histories")
-    book.set_cover("input_pdf\cover.jpg","NASA Oral Histories")
-
-    # create image from the local image
-    image_content = open("input_pdf\cover.jpg", "rb").read()
-    img = epub.EpubImage(
-        uid="image_1",
-        file_name="static/cover.jpg",
-        media_type="image/gif",
-        content=image_content,
-    )
-
-
-    # create chapter
-    c1 = epub.EpubHtml(title="Joe", file_name="chap_01.xhtml", lang="hr")
-    c1.content = (
-        "<h1>" + args.title + "</h1>"
-        "<p>" + chapter_text + "</p>"
-    )
-
-
-    # add chapter
-    book.add_item(c1)
-    # add image
-    book.add_item(img)
-
-    # define Table Of Contents
-    book.toc = (
-        epub.Link("chap_01.xhtml", "Joe Allen", "intro"),
-    )
-
-    # add default NCX and Nav file
-    book.add_item(epub.EpubNcx())
-    book.add_item(epub.EpubNav())
-
-    # define CSS style
-    style = "BODY {color: white;}"
-    nav_css = epub.EpubItem(
-        uid="style_nav",
-        file_name="style/nav.css",
-        media_type="text/css",
-        content=style,
-    )
-
-    # add CSS file
-    book.add_item(nav_css)
-
-    # basic spine
-    book.spine = ["nav", c1]
-
-    # write to the file
-    epub.write_epub(args.output, book, {})
-
+    # save the epub file
+    myBook.save(args)
 
 def get_arguments(parser):
     """
@@ -135,6 +158,10 @@ def parse_args():
     parser.add_argument("--title",
                         default="My book title",
                         help="Title of the epub",
+                        )
+    parser.add_argument("--cover_image",
+                        default=None,
+                        help="Cover image of the epub",
                         )
     parser.add_argument("--command", "-c",
                         default="single_file",
